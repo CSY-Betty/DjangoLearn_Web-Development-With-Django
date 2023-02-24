b3307144ca245d8fdb1d0657893c228fd28aa519
Betty <qwe50929@gmail.com>
2023-02-24 20:40:41 +0800

    Initial commit

---
+++ bookr/reviews/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from reviews.models import (Publisher, Contributor, Book, BookContributor, Review)
+
+class BookAdmin(admin.ModelAdmin):
+    date_hierarchy = 'publication_date'
+    list_display = ('title', 'isbn13')
+    list_filter = ('publisher', 'publication_date')
+    search_fields = ('title', 'isbn__exact', 'publisher__name__startswith')
+
+class ReviewAdmin(admin.ModelAdmin):
+    fieldsets = ((None, {'fields': ('creator', 'book')}), ('Review content', {'fields': ('content', 'rating')}))
+    exclude = ('date_edited', )
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
+    search_fields = ('last_name__startswith', 'first_name')
+    list_filter = ('last_name',)
+
+# Register your models here.
+admin.site.register(Publisher)
+admin.site.register(Contributor, ContributorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookContributor)
+admin.site.register(Review, ReviewAdmin)
